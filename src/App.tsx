564cd353a1f3b86bba3a90731c9071133918a78d
mdsas26 <mdsasayed06@gmail.com>
2026-09-14 22:18:04 +0600

    Add application components

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import TechnologySection from "./components/TechnologySection"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <TechnologySection />
+      <Footer />
+
+      <ToastContainer position="top-right" />
+    </>
+  )
+}
+
+export default App
